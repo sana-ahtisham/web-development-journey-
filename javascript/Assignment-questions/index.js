a22f50f5b8067f9ced8963dcdf278571c2582818
sana-ahtisham <sana.ahtisham2021@gmail.com>
2024-09-17 03:51:50 +0500

    add question

--- javascript/Assignment-questions/index.js
+++ javascript/Assignment-questions/index.js
@@ -4,23 +4,24 @@
 //       Assignment # 1 JAVASCRIPT ALERT
 
 
-// q:1
+// q:1  Write a script to greet your website visitor using JS alert box? 
 // var greet="WELCOME TO MY WEBSITE";
-// alert(greet)
-// q:2 
+// alert(greet);
+// q:2 Write a script to display following message on your web page ouput :error! please enter a valid pasword
 // var message="Error! Please enter a valid password";
 // alert(message);
         // or you can put message directly without using variabe
  // alert("Error! Please enter a valid password");       
-// q:3
+// q:3 Write a script to display following message on your web page: (Hint : Use line break)
 // alert("Welcome to JS land..\nHappy Coding")
-// q:4
+// q:4 Write a script to display following messages in sequence:
 // alert("Welcome to js land");
 // alert("Happy Coding");
-// q:5
+// q:5  Generate the following message through browser’s developer console:
 // console.log("Hello..I cam run JS through my web browser's console");
-// q:6
+// q:6  Make use of alerts in your new/existing HTML & CSS project.
 // alert("hello! my existing website");
+// 7. Practice placement of <script></script> element in following sections of your project in exercise 6: a. Head b. Body (before your page’s HTML) 
 
 
 //question and answer related to 2nd chapter:
