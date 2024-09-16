b779ce729f0250f8cab1daf736d713349bcf0299
sana-ahtisham <sana.ahtisham2021@gmail.com>
2024-09-17 03:31:17 +0500

    ist chapter alert questions and answer upload

--- javascript/Assignment-questions/index.js
+++ javascript/Assignment-questions/index.js
@@ -1,2 +1,35 @@
 //question and answer related to first chapter:ALERT
+
+
 //       Assignment # 1 JAVASCRIPT ALERT
+
+
+// q:1
+// var greet="WELCOME TO MY WEBSITE";
+// alert(greet)
+// q:2 
+// var message="Error! Please enter a valid password";
+// alert(message);
+        // or you can put message directly without using variabe
+ // alert("Error! Please enter a valid password");       
+// q:3
+// alert("Welcome to JS land..\nHappy Coding")
+// q:4
+// alert("Welcome to js land");
+// alert("Happy Coding");
+// q:5
+// console.log("Hello..I cam run JS through my web browser's console");
+// q:6
+// alert("hello! my existing website");
+
+
+//question and answer related to 2nd chapter:
+
+
+//       Assignment # 2 JAVASCRIPT 
+
+
+
+
+
+
