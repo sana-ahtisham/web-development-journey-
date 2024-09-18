8886b8b4e6b8e24365a728e5c13d0e264f936976
sana-ahtisham <sana.ahtisham2021@gmail.com>
2024-09-18 12:23:52 +0500

    chapter#04 LEGAL & ILLEGAL:

--- javascript/Assignment-questions/index.js
+++ javascript/Assignment-questions/index.js
@@ -1,17 +1,15 @@
 //question and answer related to first chapter:ALERT
 
-
 //       Assignment # 1 JAVASCRIPT ALERT
 
-
-// q:1  Write a script to greet your website visitor using JS alert box? 
+// q:1  Write a script to greet your website visitor using JS alert box?
 // var greet="WELCOME TO MY WEBSITE";
 // alert(greet);
 // q:2 Write a script to display following message on your web page ouput :error! please enter a valid pasword
 // var message="Error! Please enter a valid password";
 // alert(message);
-        // or you can put message directly without using variabe
- // alert("Error! Please enter a valid password");       
+// or you can put message directly without using variabe
+// alert("Error! Please enter a valid password");
 // q:3 Write a script to display following message on your web page: (Hint : Use line break)
 // alert("Welcome to JS land..\nHappy Coding")
 // q:4 Write a script to display following messages in sequence:
@@ -21,48 +19,122 @@
 // console.log("Hello..I cam run JS through my web browser's console");
 // q:6  Make use of alerts in your new/existing HTML & CSS project.
 // alert("hello! my existing website");
-// 7. Practice placement of <script></script> element in following sections of your project in exercise 6: a. Head b. Body (before your page’s HTML) 
-
+// 7. Practice placement of <script></script> element in following sections of your project in exercise 6: a. Head b. Body (before your page’s HTML)
 
 //question and answer related to 2nd chapter:
 
+//       Assignment # 2 JAVASCRIPT
+// chapter#02  VARIABLES FOR STRINGS:
 
-      Assignment # 2 JAVASCRIPT 
-chapter#02  VARIABLES FOR STRINGS:
+// q:1 Declare a variable called username?
 
-q:1 Declare a variable called username?
+// var username;
+// q:2: Declare a variable called myName & assign to it a string that represents your Full Name.
 
-var username;
-q:2: Declare a variable called myName & assign to it a string that represents your Full Name.
+// var myName="sana ahtisham";
+// q:3  Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+// var message;
+// meesage=="Hello World";
+// alert(message);
+// q:4 Write a script to save student’s bio data in JS variables and show the data in alert boxes.
+// var myName="Jhone Doe";
+// var myAge="15 Years Old";
+// var myCourse="Certified Mobile Application Developnment";
+// alert(myName);
+// alert(myAge);
+// alert(myCourse);
+// q:5 write a sript to display the following alert using one JS variable
+// var food="PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert(food);
+// q:6 declare a variable called email and assign to it a string that represent your email address(e-g example@example.com)' show the blow mentioned message in an alert box (Hint:use string concatenation)?
+// var email="san@gmail.com";
+// alert("My email address is" + " " + email);
+// q:7 declare a variable called book & give the value 'A Smarter Way to Learn Javasript'.display the following message in an alert box?
+// var book="'A Smarter Way to Learn Javasript'";
+// alert(" I am trying to learn from the Book" + "  " +  book);
+// store following string in a variable and show in alert and browser though JS
 
-var myName="sana ahtisham";
-q:3  Write script to
-a) Declare a JS variable, titled message.
-b) Assign “Hello World” to variable message
-c) Display the message in alert box.
-var message;
-meesage=="Hello World";
-alert(message);
-q:4 Write a script to save student’s bio data in JS variables and show the data in alert boxes.
-var myName="Jhone Doe";
-var myAge="15 Years Old";
-var myCourse="Certified Mobile Application Developnment";
-alert(myName);
-alert(myAge);
-alert(myCourse);
-q:5 write a sript to display the following alert using one JS variable
-var food="PIZZA\nPIZZ\nPIZ\nPI\nP";
-alert(food);
-q:6 declare a variable called email and assign to it a string that represent your email address(e-g example@example.com)' show the blow mentioned message in an alert box (Hint:use string concatenation)?
-var email="san@gmail.com";
-alert("My email address is" + " " + email);
-q:7 declare a variable called book & give the value 'A Smarter Way to Learn Javasript'.display the following message in an alert box?
-var book="'A Smarter Way to Learn Javasript'";
-alert(" I am trying to learn from the Book" + "  " +  book);
-store following string in a variable and show in alert and browser though JS
+// chapter#03 VARIABLES FOR NUMBERS:
 
+//q:1 Declare a variable called age & assign to it your age. Show your age in an alert box.
+//  var age=20;
+//  alert("I am " + age +" years old");
+//q:2 Declare & initialize a variable to keep track of how many times a visitor has visited a web page. Show his/her number of visits on your web page. For example: “You have visited this site N times”.
+// Declare a variable to keep track of the number of visits
+// var visitCount = 1;
+// var visitCountEnd="unlimited Times";
 
+// Display the visit count on the web page
+//  alert("You have visited this site " + visitCount++ + " times.");
+//  alert("You have visited this site " + visitCount++ + " times.");
+//  alert("You have visited this site " + visitCount++ + " times.");
+//  alert("You have visited this site " + visitCount++ + " times.");
+//  alert("You have visited this site " + visitCount++ + " times.");
+//  alert("You have visited this site " + visitCount++ + " times.");
+//  alert("You have visited this site " + visitCount++ + " times.");
+//  alert("You have visited this site " + visitCount++ + " times.");
+//  alert("You have visited this site " + visitCount++ + " times.");
+//  alert("You have visited this site " + visitCount++ + " times.");
+//  alert("You have visited this site " + visitCount++ + " times.");
+//  alert("You have visited this site " + visitCount++ + " times.");
+//  alert("You have visited this site " + visitCount++ + " times.");
+//  alert("You have visited this site " + visitCountEnd);
 
+//q:3 Declare a variable called birthYear and assign your birth year to it
+//   var birthYear = 1990;
+//   console.log ("My birth year is " + birthYear + ".");
+//q:4  www.xyzClothing.com . Write a script to store in variables
+//     the following information:
+//        a. Visitor’s name
+//        b. Product title
+//        c. Quantity i.e. how many products a visitor wants to
+//     order
+//     Show the following message in your browser: “John
+//     Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+//     A visitor visits an online clothing store
+// Get the visitor's name
+//   var visitorName = prompt("Please enter your name:");
 
+//    // Get the product title
+//   var productTitle = prompt("Please enter the product title:");
 
+//   // Get the quantity
+//   var quantity = +prompt("Please enter the quantity:");
 
+//  // Display the message
+//  console.log(visitorName +  " ordered " + quantity +" " +productTitle +"(s) on XYZ Clothing store." );
+
+// chapter#04 LEGAL & ILLEGAL:
+
+//   1. Declare 3 variables in one statement.
+
+//    var x = 1, y = 2, z = 3;
+//  //   or
+//  var x, y, z;
+
+//  2. Declare 5 legal & 5 illegal variable names.
+// 5 legal:
+// var firstName;
+// var $age;
+// var _address;
+// var myVar123;
+// var user_name;
+// 5 illegal:
+// var 1stName;
+// var @age;
+// var -address;
+// var my Var;
+// var user-name;
+// Q 3. Display this in your browser
+//  a) A heading stating “Rules for naming JS variables”
+//  b) Variable names can only contain ______, ______,
+//  ______ and ______.
+// For example $my_1stVariable
+// c) Variables must begin with a ______, ______ or
+// _____. For example $name, _name or name
+// d) Variable names are case _________
+// e) Variable names should not be JS _________
+// see answer in html document
