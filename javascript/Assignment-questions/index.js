4c5c3e3e84278f1bc03c595b568dc6bfaf9cd2a7
sana-ahtisham <sana.ahtisham2021@gmail.com>
2024-09-18 12:33:34 +0500

    CHAPTER # 6 ,7,8

--- javascript/Assignment-questions/index.js
+++ javascript/Assignment-questions/index.js
@@ -139,40 +139,40 @@
 // e) Variable names should not be JS _________
 // see answer in html document
 
-               //  CHAPTER 5 :MATH EXPRESSIONS
+//  CHAPTER 5 :MATH EXPRESSIONS
 
-    //  Q 1.Write a program that take two numbers & add them in a
-    //  new variable. Show the result in your browser.
-    //  var num1=+prompt("ENTER NUMBER 1");
-    //  var num2=+prompt("ENTER NUMBER 2");
-    //  var sum=num1+num2;
-    // alert("THE addition OF THESE TWO NUMBER ARE:" +sum);
-    //  console.log(sum);
-    // Q:2. Repeat task1 for subtraction, multiplication, division &
-    // modulus.
-    //  var num1=+prompt("ENTER NUMBER 1");
-    //  var num2=+prompt("ENTER NUMBER 2");
-    //  var sub=num1-num2;
-    //  alert("THE subtraction OF THESE TWO NUMBER ARE:" + sub);
-    //  console.log(sub);
+//  Q 1.Write a program that take two numbers & add them in a
+//  new variable. Show the result in your browser.
+//  var num1=+prompt("ENTER NUMBER 1");
+//  var num2=+prompt("ENTER NUMBER 2");
+//  var sum=num1+num2;
+// alert("THE addition OF THESE TWO NUMBER ARE:" +sum);
+//  console.log(sum);
+// Q:2. Repeat task1 for subtraction, multiplication, division &
+// modulus.
+//  var num1=+prompt("ENTER NUMBER 1");
+//  var num2=+prompt("ENTER NUMBER 2");
+//  var sub=num1-num2;
+//  alert("THE subtraction OF THESE TWO NUMBER ARE:" + sub);
+//  console.log(sub);
 
-    //  var num1=+prompt("ENTER NUMBER 1");
-    //  var num2=+prompt("ENTER NUMBER 2");
-    //  var division=num1/num2;
-    //  alert("THE division OF THESE TWO NUMBER ARE:" + division);
-    //  console.log(division);
-     
-    //  var num1=+prompt("ENTER NUMBER 1");
-    //  var num2=+prompt("ENTER NUMBER 2");
-    //  var multiply=num1*num2;
-    // alert("THE multiplcation OF THESE TWO NUMBER ARE:" +multiply);
-    //  console.log(multiply);
+//  var num1=+prompt("ENTER NUMBER 1");
+//  var num2=+prompt("ENTER NUMBER 2");
+//  var division=num1/num2;
+//  alert("THE division OF THESE TWO NUMBER ARE:" + division);
+//  console.log(division);
 
-    //  var num1=+prompt("ENTER NUMBER 1");
-    //  var num2=+prompt("ENTER NUMBER 2");
-    //  var modulus=num1 % num2;
-    //  alert("THE  remainder OF THESE TWO NUMBER ARE:" +modulus);
-    //  console.log(modulus);         
+//  var num1=+prompt("ENTER NUMBER 1");
+//  var num2=+prompt("ENTER NUMBER 2");
+//  var multiply=num1*num2;
+// alert("THE multiplcation OF THESE TWO NUMBER ARE:" +multiply);
+//  console.log(multiply);
+
+//  var num1=+prompt("ENTER NUMBER 1");
+//  var num2=+prompt("ENTER NUMBER 2");
+//  var modulus=num1 % num2;
+//  alert("THE  remainder OF THESE TWO NUMBER ARE:" +modulus);
+//  console.log(modulus);
 
 // Q:3:  Do the following using JS Mathematic Expressions:
 // a. Declare a variable.
@@ -183,90 +183,89 @@
 // answer is undefined
 
 // c. Initialize the variable with some number.
-  //  myVar = 5;
+//  myVar = 5;
 //// d. Show the value of variable in your browser like “Initial value: 5”.
 //  document.write("Initial value: " + myVar + "<br>");
 // e. Increment the variable.
-        // myVar++;
+// myVar++;
 // f. Show the value of variable in your browser like “Value after increment is:
-  // document.write("value after increment is:" + myVar + "<br>");
-    // g. Add 7 to the variable.
-    // myVar += 7;
-    // console.log(myVar);
-    // h. Show the value of variable in your browser like “Value after addition is:
-    // document.write("Value after addition is: " + myVar + "<br>");
-    // value of variable in your browser like “Value after addition is: 13”.
-    // i. Decrement the variable.
-    // myVar--;
+// document.write("value after increment is:" + myVar + "<br>");
+// g. Add 7 to the variable.
+// myVar += 7;
+// console.log(myVar);
+// h. Show the value of variable in your browser like “Value after addition is:
+// document.write("Value after addition is: " + myVar + "<br>");
+// value of variable in your browser like “Value after addition is: 13”.
+// i. Decrement the variable.
+// myVar--;
 
-    // j. Show the value of variable in your browser like “Value
-    // after decrement is: 12”.
-    // document.write("Value after decrement is: " + myVar + "<br>");
-    // // k. Show the remainder after dividing the variable’s value by 3.
-    //  var remainder = myVar % 3;
-    //  document.write("The remainder is : 0" + remainder +"<br>");
+// j. Show the value of variable in your browser like “Value
+// after decrement is: 12”.
+// document.write("Value after decrement is: " + myVar + "<br>");
+// // k. Show the remainder after dividing the variable’s value by 3.
+//  var remainder = myVar % 3;
+//  document.write("The remainder is : 0" + remainder +"<br>");
 
-    //  Q: 4. Cost of one movie ticket is 600 PKR. Write a script to
-    //  store ticket price in a variable & calculate the cost of buying 5
-    //  tickets to a movie. Example output:
-    // var movieTicket=600;
-    // var buyingTickets =movieTicket*5;
-    // document.write("Total cost to buy 5 tickets to a movie is " + buyingTickets +"<br>");
+//  Q: 4. Cost of one movie ticket is 600 PKR. Write a script to
+//  store ticket price in a variable & calculate the cost of buying 5
+//  tickets to a movie. Example output:
+// var movieTicket=600;
+// var buyingTickets =movieTicket*5;
+// document.write("Total cost to buy 5 tickets to a movie is " + buyingTickets +"<br>");
 //     5. Write a script to display multiplication table of any
 // number in your browser. E.g
 // var num=5;
-                  //  OR
+//  OR
 // var num=+prompt("enter any table and you will get solution");
 // var i;
 // for(i=1;i<=10;i++){
 //     document.write(num + "x" + i + "=" + num*i + "<br>")
 // }
-    // 6. The Temperature Converter: It's hot out! Let's make a
-    // converter based on the steps here.
-    // a. Store a Celsius temperature into a variable.
-    // b. Convert it to Fahrenheit & output “NNoC is NNoF”.
-    // c. Now store a Fahrenheit temperature into a variable.
-    // d. Convert it to Celsius & output "NNoF is NNoC".
-       // Conversion Formulae:
-    // var celsius = 25;
-    // var fahrenheit = (celsius * 9/5) + 32;
-    // document.write(celsius + "°C is " + fahrenheit + "°F" +"<br>");
-    // var fahrenheit = 70;
-    // var celsius = (fahrenheit - 32) * 5/9;
-    // document.write(fahrenheit + "°F is " + celsius + "°C" +"<br>");
+// 6. The Temperature Converter: It's hot out! Let's make a
+// converter based on the steps here.
+// a. Store a Celsius temperature into a variable.
+// b. Convert it to Fahrenheit & output “NNoC is NNoF”.
+// c. Now store a Fahrenheit temperature into a variable.
+// d. Convert it to Celsius & output "NNoF is NNoC".
+// Conversion Formulae:
+// var celsius = 25;
+// var fahrenheit = (celsius * 9/5) + 32;
+// document.write(celsius + "°C is " + fahrenheit + "°F" +"<br>");
+// var fahrenheit = 70;
+// var celsius = (fahrenheit - 32) * 5/9;
+// document.write(fahrenheit + "°F is " + celsius + "°C" +"<br>");
 
-    // Q 7. Write a program to implement checkout process of a
-    // shopping cart system for an e-commerce website. Store
-    // the following in variables
-    // a. Price of item 1
-    // b. Price of item 2
-    // c. Ordered quantity of item 1
-    // d. Ordered Quantity of item 2
-    // e. Shipping charges
+// Q 7. Write a program to implement checkout process of a
+// shopping cart system for an e-commerce website. Store
+// the following in variables
+// a. Price of item 1
+// b. Price of item 2
+// c. Ordered quantity of item 1
+// d. Ordered Quantity of item 2
+// e. Shipping charges
 
-
-    // Compute the total cost & show the receipt in your browser.
-    // Sample data: Item 1 Price: 250; Item 2 Price: 100
-    // Quantity of Item 1: 3; Quantity of Item 2: 7;
-    // Shipping Charges: 100. Total Cost of your order: 1000
-    // var item1Price = 650;
-    // var item2Price = 100;
-    // var item1Quantity = 3;
-    // var item2Quantity = 7;
-    // var shippingCharges = 100;
-    // document.write("Price of item 1 is:" + item1Price +"<br>");
-    // document.write("Price of item 2 is:" + item2Price+"<br>");
-    // document.write("quantity of item 1 is:" + item1Quantity+"<br>");
-    // document.write("quantity of item 2 is:" + item2Quantity+"<br>");
-    // var totalCost = (item1Price * item1Quantity) + (item2Price *item2Quantity) +shippingCharges;
-    // document.write("Total Cost of your order:" + totalCost+"<br>");
-  //Q :8.   Store total marks & marks obtained by a student   in 2 variables. Compute the percentage & show the result in your browser
-  // var totalMarks = 980;
-  // var marksObtained = 804;
-  // var percentage = (marksObtained/totalMarks)*100;
-  // document.write("Total Marks:" + totalMarks +"<br>");
-  // document.write("Marks Obtained:" + marksObtained +"<br>");
-  // document.write("Percentage:" + percentage + "%" +"<br>");
+// Compute the total cost & show the receipt in your browser.
+// Sample data: Item 1 Price: 250; Item 2 Price: 100
+// Quantity of Item 1: 3; Quantity of Item 2: 7;
+// Shipping Charges: 100. Total Cost of your order: 1000
+// var item1Price = 650;
+// var item2Price = 100;
+// var item1Quantity = 3;
+// var item2Quantity = 7;
+// var shippingCharges = 100;
+// document.write("Price of item 1 is:" + item1Price +"<br>");
+// document.write("Price of item 2 is:" + item2Price+"<br>");
+// document.write("quantity of item 1 is:" + item1Quantity+"<br>");
+// document.write("quantity of item 2 is:" + item2Quantity+"<br>");
+// var totalCost = (item1Price * item1Quantity) + (item2Price *item2Quantity) +shippingCharges;
+// document.write("Total Cost of your order:" + totalCost+"<br>");
+//Q :8.   Store total marks & marks obtained by a student   in 2 variables. Compute the percentage & show the result in your browser
+// var totalMarks = 980;
+// var marksObtained = 804;
+// var percentage = (marksObtained/totalMarks)*100;
+// document.write("Total Marks:" + totalMarks +"<br>");
+// document.write("Marks Obtained:" + marksObtained +"<br>");
+// document.write("Percentage:" + percentage + "%" +"<br>");
 //   Q :9. Assume we have 10 US dollars & 25 Saudi Riyals. Write a
 // script to convert the total currency to Pakistani Rupees.
 // Perform all calculations in a single expression.
@@ -335,6 +334,110 @@
 // document.write("you will need " + totalSnack + " " + snack + " to last you until the ripe old age of " + maxAge +"<br>");
 // NNNN to last you until the ripe old age of NN”.
 
+// CHAPTER # 1 TO 5 SOLUTIONS THE END
 
-          // CHAPTER # 1 TO 5 SOLUTIONS THE END
+// CHAPTER # 6 :MATH EXPRESSIONS
+//CHAPTER # 7:Math expressions: Eliminating ambiguity
+//CHAPTER # 8:Math expressions: Concatenating text strings
 
+
+//   Assignment # 6-8
+//   JAVASCRIPT solution
+
+//Q 1. Write a program to take a number in a variable, do the
+// required arithmetic to display the following result in your
+// browser:
+// var a = 10;
+
+// document.write("The value of a is : " +  a  +"<br>");
+
+// document.write("The value of ++a is" +  ++a +"<br> Now the value of a is : " + a +"<br>");
+// document.write("The value of a++ is" +  a++ +"<br> Now the value of a is : " + a +"<br>");
+// document.write("The value of --a is" +  --a +"<br> Now the value of a is : " + a +"<br>");
+// document.write("The value of a-- is" +  a-- +"<br> Now the value of a is : " + a +"<br>");
+
+// Q: 2. What will be the output in variables a, b & result after
+// execution of the following script:
+// var a = 2, b = 1;
+// var result = --a - --b + ++b + b--;
+// Explain the output at each stage:
+// --a;
+// --a - --b;
+// --a - --b + ++b;
+// --a - --b + ++b + b--;
+
+// var a = 2, b = 1;
+// document.write("--a ; a become 1.")
+// document.write("<br>--a - --b ; a become 1 and b become 0 so result is 1 - 0 = 1");
+// document.write("<br> --a - --b + ++b ; a is 1 . b is 0 , then incremented to 1.Result is 1 - 0 + 1 = 2 ");
+// document.write("<br> --a - --b + ++b + b-- ; a is 1 . b is 0 , incremented to 1 , and used as 1 before decrementing . Result is 1 - 0 + 1 + 1 = 3 . After this operation , b becomes 0 . Thus, the final values are : <br>")
+// document.write(--a - --b + ++b + b--);
+
+// a = 1 , b = 0 , result = 3
+// Q:3.Write a program that takes input a name from user &
+// greet the user.
+// var yourName=prompt("ENTER YOUR NAME");
+// document.write("Hello " + yourName + " !");
+
+// 5. Write a program to take input a number from user &
+// display it’s multiplication table on your browser. If user
+// does not enter a new number, multiplication table of 5
+// should be displayed by default.
+// var a = prompt("Enter a number");
+// if (a === null || a === "") {
+//     a = 5;
+// } else {
+//     a = a;
+// }
+
+// document.write("Table of " + a + "<br>");
+// for (var i = 1; i <= 10; i++) {
+//     document.write(a + " x " + i + " = " + (a * i) + "<br>");
+// }
+// Q :6. Take
+// a) Take three subjects name from user and store them in 3
+// different variables.
+// b) Total marks for each subject is 100, store it in another
+// variable.
+// c) Take obtained marks for first subject from user and
+// stored it in different variable.
+// d) Take obtained marks for remaining 2 subjects from user
+// and store them in variables.
+// e) Now calculate total marks and percentage and show the
+// result in browser like this.(Hint: user table)
+// a) Take three subjects name from user and store them in 3 different variables
+//  var subject1 = prompt("Enter the name of the first subject");
+//  var subject2 = prompt("Enter the name of the second subject");
+//  var subject3 = prompt("Enter the name of the third subject");
+
+// b) Total marks for each subject is 100, store it in another variable
+//  var totalMarksPerSubject = 100;
+
+// c) Take obtained marks for first subject from user and store it in different variable
+//  var obtainedMarks1 = +prompt("Enter obtained marks for " + subject1);
+
+// d) Take obtained marks for remaining 2 subjects from user and store them in variables
+//  var obtainedMarks2 =+prompt("Enter obtained marks for " + subject2);
+//  var obtainedMarks3 =+prompt("Enter obtained marks for " + subject3);
+
+// e) Calculate total marks and percentage
+//  var totalObtainedMarks = obtainedMarks1 + obtainedMarks2 + obtainedMarks3;
+//  var totalMaximumMarks = totalMarksPerSubject * 3;
+//  var percentage = (totalObtainedMarks / totalMaximumMarks) * 100;
+
+// Display the result in a table
+//  document.write("<table border='1'>");
+//  document.write("<tr><th>Subject</th><th>Obtained Marks</th><th>Total Marks</th></tr>");
+//  document.write("<tr><td>" + subject1 + "</td><td>" + obtainedMarks1 + "</td><td>" + totalMarksPerSubject + "</td></tr>");
+//  document.write("<tr><td>" + subject2 + "</td><td>" + obtainedMarks2 + "</td><td>" + totalMarksPerSubject + "</td></tr>");
+//  document.write("<tr><td>" + subject3 + "</td><td>" + obtainedMarks3 + "</td><td>" + totalMarksPerSubject + "</td></tr>");
+//  document.write("<tr><th>Total</th><th>" + totalObtainedMarks + "</th><th>" + totalMaximumMarks + "</th></tr>");
+//  document.write("<tr><th>Percentage</th><th colspan='2'>" + percentage.toFixed(2) + "%</th></tr>");
+//  document.write("</table>");
+
+// Display the result  of % of subjects in a break line:
+
+// document.write("Subject1 : " + subject1 + "<br>Obtained Marks : "
+//    + sub1Obtained + "<br>Total Marks : " + totalMarks + "<br>Percentage : "+ percentage1 + "<br>Subject2 : " + subject2 +
+//     "<br>Obtained Marks : " + sub2Obtained + "<br>Total Marks : " + totalMarks + "<br>Percentage2 : " +percentage2 + "<br>< br>Subject3 : " + subject3 + "<br>Obtained Marks : "
+//    + sub3Obtained + "<br>Total Marks : " + totalMarks + "<br>Percentage3 : " + percentage3 + "<br><br>Total : " + total + "<br>Percentage : " + percentage + "<br> Grade : " + percentage + "<br>Remarks : " + percentage + "<br><br>"
